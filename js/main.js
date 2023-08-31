d621da52fd295f99eeec986a9507657c3842de51
sabirlkhaloufi <sabirkhaloufi@gmail.com>
2023-09-01 00:09:01 +0100

    add dark mode

--- js/main.js
+++ js/main.js
@@ -1,11 +1,52 @@
 $("#closemenu").click(function(){
-    $(".menulink").addClass("hide-menu");
+    $(".menulink").removeClass("show-menu");
 })
 
 $("#showmenu").click(function(){
-    $(".menulink").removeClass("hide-menu");
+    $(".menulink").addClass("show-menu");
 })
 
 
+
+
+const darkmode = ()=>{
+    $(".mode").addClass("darkmode");
+    $(".fa-moon").addClass("d-none");
+    $(".fa-lightbulb").removeClass("d-none");
+    localStorage.setItem("mode","dark")
+    $("#mode").addClass("dark")
+    $("#mode").removeClass("light")
+}
+
+const lightmode = ()=>{
+    $(".mode").removeClass("darkmode");
+    $(".fa-moon").removeClass("d-none");
+    $(".fa-lightbulb").addClass("d-none");
+    localStorage.setItem("mode","light")
+    $("#mode").removeClass("dark")
+    $("#mode").addClass("light")
+}
+
+let mode = localStorage.getItem("mode");
+if(mode == "dark"){
+darkmode()
+}
+
+if(mode == "light"){
+    lightmode()
+}
+
+
+
+$(".fa-moon").click(function(){
+    darkmode();
+})
+
+$(".fa-lightbulb").click(function(){
+    lightmode(); 
+})
+
+
+// dark mode
 
 
